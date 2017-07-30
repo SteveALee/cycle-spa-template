b96a00cbedb4ed3d8493acd1485a4c4d28794381
Steve Lee <steve@opendirective.com>
2017-07-30 21:16:56 +0100

    Add code from brian repo

---
+++ src/app.tsx
@@ -0,0 +1,56 @@
+import xs, { Stream } from 'xstream'
+import { VNode, DOMSource } from '@cycle/dom'
+import { StateSource } from 'cycle-onionify'
+import isolate from '@cycle/isolate'
+
+import { DriverSources, DriverSinks, Component } from './drivers'
+import { routes, initialRoute } from './routes'
+
+import { State as p1State } from './pages/page1'
+import { State as p2State } from './pages/page2'
+export interface State {
+    thing: number
+    'page_/': p1State
+    'page_/p2': p2State
+}
+export type Reducer = (prev?: State) => State | undefined
+export type Sources = DriverSources & { onion: StateSource<State> }
+export type Sinks = DriverSinks & { onion: Stream<Reducer> }
+
+export function App(sources: Sources): Sinks {
+    const state$ = sources.onion.state$
+    const initReducer$ = xs.of(
+        (prevState: State): State =>
+            typeof prevState === 'undefined'
+                ? {
+                      thing: 123,
+                      'page_/': { count: 0 },
+                      'page_/p2': { count: 10 }
+                  }
+                : prevState
+    )
+
+    const match$ = sources.router.define(routes)
+
+    const pageSinks$ = match$.map(
+        ({ path, value: page }: { path: string; value: Component }) => {
+            return isolate(page, `page_${path}`)(
+                Object.assign({}, sources, {
+                    router: sources.router.path(path)
+                })
+            )
+        }
+    ) // no need to remember?
+
+    const pageReducer$ = pageSinks$
+        .map((ps: Sinks) => ps.onion)
+        .flatten() as Stream<Reducer>
+
+    return {
+        // TODO remove explicit declaration of sink keys if possible
+        DOM: pageSinks$.map((ps: Sinks) => ps.DOM).flatten(),
+        onion: xs.merge(initReducer$, pageReducer$),
+        speech: pageSinks$.map((ps: Sinks) => ps.speech).flatten(),
+        router: pageSinks$.map((ps: Sinks) => ps.router).flatten()
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { setup, run } from '@cycle/run'
+import { rerunner } from 'cycle-restart'
+import isolate from '@cycle/isolate'
+import onionify from 'cycle-onionify'
+import storageify from 'cycle-storageify'
+
+import { mkDrivers, Component } from './drivers'
+import { App } from './app'
+
+// TODO PR upstream - 2nd,3rd options should be optional
+declare type StorageifyOptions = {
+    key: string
+    serialize(state: any): string
+    deserialize(stateStr: string): any
+}
+
+const main: Component = onionify(
+    storageify(App, { key: 'brian-state' } as StorageifyOptions)
+)
+
+/// #if PRODUCTION
+run(main as any, mkDrivers())
+
+/// #else
+
+const rerun = rerunner(setup, mkDrivers, isolate)
+rerun(main as any)
+
+if (module.hot) {
+    module.hot.accept('./app', () => {
+        const newApp = require('./app').App
+
+        rerun(onionify(newApp))
+    })
+}
+/// #endif
